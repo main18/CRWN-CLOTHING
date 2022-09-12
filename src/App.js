053dc3e83b50d256028fa6236ed42a0f8e692f12
main18 <ayman.bmh123@gmail.com>
2022-09-12 22:11:41 +0200

    implementing routing and adding navigation bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import Directory from "./components/directory/directory.component";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import { Routes, Route } from "react-router-dom";
+
+const Shop = () => {
+  return <h1>I am the Shop Page</h1>;
+};
 
 const App = () => {
-  return <Directory />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
