7b3db03a6807e5fe982efeed8121d7f9c9bb7adf
main18 <ayman.bmh123@gmail.com>
2022-09-13 17:48:21 +0200

    creating user from authentification

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -13,15 +13,16 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
 export const auth = getAuth();
 
-export const signinWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signinWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
 
 export const db = getFirestore();
 
